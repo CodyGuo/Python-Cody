f7046fe883472d5c130bdc398f5d03764097e9fb
CodyGuo <guoxiao219@126.com>
2017-06-01 00:16:09 +0800

    add  fib

---
+++ python/examples/2017/05/31/fib_go.go
@@ -0,0 +1,54 @@
+/*
+	Fibonacci(40) = 102334155
+	计算Fibonacci用时 --> 1.7851877s
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var ok bool
+
+// 斐波那契数列
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	var n = 40
+	var err error
+	if len(os.Args) == 2 {
+		n, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	start := time.Now()
+	fibN := fib(n)
+	ok = true
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+	fmt.Printf("计算Fibonacci用时 --> %s\n", time.Since(start))
+}
+
+// 打印动态
+func spinner(delay time.Duration) {
+	if !ok {
+		fmt.Print("正在计算中...  ")
+		for {
+			for _, r := range `-\|/` {
+				fmt.Printf("\b%c", r)
+				time.Sleep(delay)
+			}
+		}
+	}
+}
---
+++ python/examples/2017/05/31/fib_py.py
@@ -0,0 +1,20 @@
+# -*- coding=utf-8 -*-
+import time
+
+'''
+    Fibonacci(40) = 102334155
+    计算Fibonacci用时 182.90810322761536 s
+'''
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    n= 40
+    fibN = fib(n)
+    end = time.time()
+    print('Fibonacci(%d) = %d' % (n, fibN))
+    print('计算Fibonacci用时 %s s' % (end - start))
